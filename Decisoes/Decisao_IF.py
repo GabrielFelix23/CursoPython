761725b7d461ef5af274feddaf05a893661de941
GabrielFelix23 <srgfelix123@gmail.com>
2020-12-05 06:06:01 -0300

    Decisoes encadeadas

--- Decisoes/Decisao_IF.py
+++ Decisoes/Decisao_IF.py
@@ -1,7 +1,12 @@
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
+tipo_Doencas = input("Suspeita de doenças infectocontagiosa?")
 
 if idade >= 65:
     print("O paciente " + nome + " possui atendimento!")
+
+elif tipo_Doencas == "SIM":
+    print("O paciente " + nome + " deve ser direcionado para sala de espera reservada.")
+
 else:
     print("O paciente " + nome + " não possui atendimento!")
